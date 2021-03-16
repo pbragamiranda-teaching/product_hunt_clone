b479989c43ee9c031d2c2065b6218f87f37d9fd5
Pedro Miranda <pbragamiranda@gmail.com>
2021-03-16 10:25:05 -0300

    new commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
+  resource :products, only: [:new, :create]
 end
+
+# localhost:3000/
+
+# localhost:3000/top-product
+# visit '/top-product'
+# visit top_product_path
